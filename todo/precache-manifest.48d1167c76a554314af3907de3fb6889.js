e2eab37b3f9f7d9fbec3d764d9ace4237ae224be
jevons <36203025+jevonsofcode@users.noreply.github.com>
2019-10-17 13:53:02 +0800

    add a test html

---
+++ todo/precache-manifest.48d1167c76a554314af3907de3fb6889.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365a98b2a333869cfb35a01d21c60ea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ee4ff4002f6e60c25a",
+    "url": "/static/js/2.5129a158.chunk.js"
+  },
+  {
+    "revision": "85b2d2f70b809c4ce0ec",
+    "url": "/static/js/main.056a6d85.chunk.js"
+  },
+  {
+    "revision": "2188f29ab47ed0638d8f",
+    "url": "/static/js/runtime-main.ebf04d0b.js"
+  }
+]);
